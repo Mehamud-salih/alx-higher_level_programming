d4afae8114c3175a1428cee55f7fd709ed9d7cce
Mehamud-salih <mahmudsalih96@gmail.com>
2022-06-21 18:25:33 +0300

    corrected

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
-# 101-safe_function.py
-# Brennan D Baraban <375@holbertonschool.com>
-
-import sys
-
-
-def safe_function(fct, *args):
-    """Executes a function safely.
+def safe_print_list(my_list=[], x=0):
+    """Print x elememts of a list.
     Args:
-        fct: The function to execute.
-        args: Arguments for fct.
+        my_list (list): The list to print elements from.
+        x (int): The number of elements of my_list to print.
     Returns:
-        If an error occurs - None.
-        Otherwise - the result of the call to fct.
+        The number of elements printed.
     """
-    try:
-        result = fct(*args)
-        return (result)
-    except:
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (None)
+    ret = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            ret += 1
+        except IndexError:
+            break
+    print("")
+    return (ret)
