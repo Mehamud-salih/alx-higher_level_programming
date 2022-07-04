603cecb7dbc8171831ef3d08b73c2a23cdaf292b
Mehamud-salih <mahmudsalih96@gmail.com>
2022-07-04 19:49:59 +0300

    python inheritance

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
-"""This module creates class Square which inherits from BaseGeometry"""
+"""Square module.
+Contains a class Square that inherits from
+Rectangle and some methods.
+"""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Class Square inherits from Rectangle"""
+    """Defines the Square class that inherits from Rectangle."""
+
     def __init__(self, size):
-        """Instantiate private instance field size"""
-        super().integer_validator("size", size)
+        """Checks and sets the default attributes of Square class."""
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
+    def __str__(self):
+        """Sets the str behaviour."""
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
